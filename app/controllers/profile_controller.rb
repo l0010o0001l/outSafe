d5439bf91b8098b48eac4be45f4de0399308e45a
l0010o0001l <tara.planas@gmail.com>
2016-02-09 23:18:23 -0800

    Strong params for profile attributes

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,16 +1,22 @@
+# @todo Write tests
 class ProfileController < ApplicationController
   before_action :authenticate_user!
 
   def update
     @profile = Profile.find_by(user_id: current_user[:id])
-    @profile.update!
+    @profile.update(profile_params)
     redirect_to profile_path, notice: "Profile successfully updated!"
   end
 
   def destroy
     @user = User.find(params[:id])
     if @user.destroy
-     redirect_to root_url, notice: "User deleted."
+     redirect_to root_url, notice: "We're sorry to see you go. :("
     end
+  end
+
+  def profile_params
+    params.permit!
+    profile_attributes = params["profile"]
   end
 end
