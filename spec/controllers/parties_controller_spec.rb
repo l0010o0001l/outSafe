6ad2f39b1c8f199e1a53f9d1b7ac04d0324cf0ca
l0010o0001l <tara.planas@gmail.com>
2016-02-15 20:50:02 -0800

    PartiesController tests refactor

--- spec/controllers/parties_controller_spec.rb
+++ spec/controllers/parties_controller_spec.rb
@@ -3,9 +3,27 @@ require 'rails_helper'
 RSpec.describe PartiesController, type: :controller do
   include Devise::TestHelpers
 
-  let(:current_user) { FactoryGirl.build_stubbed :user }
-  let(:party) { FactoryGirl.build_stubbed :party }
+  let(:user) { FactoryGirl.build_stubbed :user }
+  let(:party) { FactoryGirl.create :party }
+  let(:profile) { FactoryGirl.build_stubbed :god_profile }
+  let(:valid_party_params) do
+    {
+      :user_id => user.id,
+      :title => "YOLO #SWAG",
+      :address => "493 SW Broadway, Portland, OR 97205",
+      :min_age => 21
+    }
+  end
+  let(:invalid_party_params) do
+    {
+      :user_id => nil,
+      :title =>  89532,
+      :address => 911,
+      :min_age => "21"
+    }
+  end
 
+  # Successful get requests
   context "success" do
     describe "GET #index" do
       it "returns a 200 status code" do
@@ -19,6 +37,12 @@ RSpec.describe PartiesController, type: :controller do
       end
     end
 
+    describe "GET #edit" do
+      it "returns a 200 status code" do
+        expect(response.status).to eq 200
+      end
+    end
+
     describe "GET #show" do
       it "returns a 200 status code" do
         expect(response.status).to eq 200
@@ -26,54 +50,40 @@ RSpec.describe PartiesController, type: :controller do
     end
   end
 
+  # Successful POST requests
   context "success" do
-    describe "GET #edit" do
-      it "returns a 200 status code" do
-        expect(response.status).to eq 200
+    describe "POST #create" do
+      before do
+        subject { POST :create, valid_party_params }
       end
-    end
-  end
 
-  describe "POST #create" do
-    before do
-      subject { POST :create, party_params }
-    end
-
-    context "success" do
-      it "redirects to the created party" do
-        expect(response.status).to eq 200
-      end
-    end
-
-    context "failure" do
-      it "redirects to new party form" do
-        expect(response.status).to eq 200
+      context "success" do
+        it "has a response status of 200" do
+          expect(response.status).to eq 200
+        end
       end
     end
   end
 
   describe "PUT #update" do
-    before do
-      subject { PUT :update, party_params }
-    end
-
     context "success" do
       it "redirects to the updated party" do
         expect(response.status).to eq 200
       end
     end
+  end
 
-    context "failure" do
-      it "redirects to the edit page" do
-        # expect(subject).to redirect_to :edit
+  describe "PATCH #update" do
+    context "success" do
+      it "redirects to the updated party" do
+        expect(response.status).to eq 200
       end
     end
   end
 
   describe "DELETE #destroy" do
     it "redirects to parties" do
-      # expect(subject).to redirect_to parties_url
+      expect(response.status).to eq 200
     end
   end
-
 end
